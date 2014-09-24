a41a6da67872b1102fc7a33e7a8362bd17f8804e
HIGUCHI Daisuke <d-higuchi@creationline.com>
2014-09-24 17:58:13 +0900

    support loading Gemfile. closes #12

--- lib/busser/runner_plugin/serverspec.rb
+++ lib/busser/runner_plugin/serverspec.rb
@@ -25,9 +25,22 @@ require 'busser/runner_plugin'
 class Busser::RunnerPlugin::Serverspec < Busser::RunnerPlugin::Base
   postinstall do
     install_gem("serverspec")
+    install_gem("bundler")
   end
 
   def test
+    # Referred from busser-shindo
+    gemfile_path = File.join(suite_path, 'serverspec', 'Gemfile')
+    if File.exists?(gemfile_path)
+      # Bundle install local completes quickly if the gems are already found locally
+      # it fails if it needs to talk to the internet. The || below is the fallback
+      # to the internet-enabled version. It's a speed optimization.
+      banner('Bundle Installing..')
+      ENV['PATH'] = [ENV['PATH'], Gem.bindir].join(':')
+      bundle_option = "--gemfile #{gemfile_path}"
+      run("bundle install --local #{bundle_option} || bundle install #{bundle_option}")
+    end
+
     runner = File.join(File.dirname(__FILE__), %w{.. serverspec runner.rb})
 
     run_ruby_script!("#{runner} #{suite_path('serverspec').to_s}")
